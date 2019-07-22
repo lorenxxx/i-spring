18fd93f0e9825760d9a4c9f1d9a6a1b3c592a5ae
loren <liangliangpro@outlook.com>
2019-07-22 17:59:29 +0800

    feat: IoC流程

--- src/main/java/com/us/improve/spring/ioc/Main.java
+++ src/main/java/com/us/improve/spring/ioc/Main.java
@@ -5,8 +5,8 @@ import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
 
 /**
- * @ClassName UseIOC
- * @Desciption TODO
+ * @ClassName Main
+ * @Desciption 编程式使用IoC容器
  * @Author Loren
  * @Date 2018/12/8 14:56
  * @Version 1.0
